2682a62395269a9acc748fcd0e90ab04c58df921
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2015-11-26 08:58:49 +0800

    Modify -generate-random-data to -generate-data which produces a more realistic activity pattern
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var generateRandomData = flag.Bool("generate-random-data", false, "Generates random data in JSON format")
+var generateData = flag.Bool("generate-data", false, "Generates data in JSON format")
 
 func dataEndpoint(w http.ResponseWriter, r *http.Request) {
 	var handler dataHandler
@@ -46,8 +46,8 @@ func bodiesEndpoint(w http.ResponseWriter, r *http.Request) {
 func main() {
 	flag.Parse()
 
-	if *generateRandomData {
-		doGenerateRandomData()
+	if *generateData {
+		doGenerateData()
 		return
 	}
 
@@ -62,17 +62,45 @@ func main() {
 	n.Run(":8080")
 }
 
-func doGenerateRandomData() {
+func doGenerateData() {
+	var hourlyActivities = [24]int{
+		0, // 12AM
+		0, // 1AM
+		0, // 2AM
+		0, // 3AM
+		0, // 4AM
+		0, // 5AM
+		0, // 6AM
+		1, // 7AM
+		2, // 8AM
+		9, // 9AM
+		4, // 10AM
+		2, // 11AM
+		5, // 12PM
+		8, // 1PM
+		4, // 2PM
+		3, // 3PM
+		2, // 4PM
+		2, // 5PM
+		1, // 6PM
+		2, // 7PM
+		2, // 8PM
+		1, // 9PM
+		0, // 10PM
+		0, // 11PM
+	}
+
+	rand.Seed(time.Now().Unix())
+
 	var dps [minutesPerDay][2]int
 	enters := 0
 	exits := 0
-
-	rand.Seed(time.Now().Unix())
-	for i := range dps {
-		enters += rand.Int() % 4
-		exits += rand.Int() % 3
-		dps[i][0] = enters
-		dps[i][1] = exits
+	for minute := range dps {
+		hour := minute / 60
+		enters += rand.Int() % 3 * hourlyActivities[hour]
+		exits += rand.Int() % 3 * hourlyActivities[hour]
+		dps[minute][0] = enters
+		dps[minute][1] = exits
 	}
 
 	jsonData, err := json.Marshal(dps)
